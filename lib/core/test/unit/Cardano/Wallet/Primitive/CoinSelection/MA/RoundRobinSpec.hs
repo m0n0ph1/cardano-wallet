ace76e525e38ebebe523ec72735a13fe8b0e7daa
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2021-05-28 17:38:30 +1000

    Fix comment interpreted as Haddock syntax error

--- lib/core/test/unit/Cardano/Wallet/Primitive/CoinSelection/MA/RoundRobinSpec.hs
+++ lib/core/test/unit/Cardano/Wallet/Primitive/CoinSelection/MA/RoundRobinSpec.hs
@@ -1048,7 +1048,7 @@ prop_assetSelectionLens_givesPriorityToSingletonAssets (Blind (Small u)) =
         case mUpdatedState of
             Nothing -> do
                 -- This should never happen: we should always be able to select
-                -- *something* that matches.
+                -- _something_ that matches.
                 monitor $ counterexample "Error: unable to select any entry"
                 assert False
             Just SelectionState {selected} -> do
@@ -1080,7 +1080,7 @@ prop_coinSelectionLens_givesPriorityToCoins (Blind (Small u)) =
         case mUpdatedState of
             Nothing -> do
                 -- This should never happen: we should always be able to select
-                -- *something* that matches.
+                -- _something_ that matches.
                 monitor $ counterexample "Error: unable to select any entry"
                 assert False
             Just SelectionState {selected} -> do
